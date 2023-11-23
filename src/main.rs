fc741fe41d37a338745d8e493a9509d51247d37e
khai96_ <hvksmr1996@gmail.com>
2023-11-23 18:31:42 +0700

    add fibonacci

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,13 @@ fn main() {
     }
 }
 
-const MAX: u32 = 500;
+const MAX: u64 = 500;
 
-fn data(dir: PathBuf) -> impl Fn(u32) -> (PathBuf, String) {
+fn data(dir: PathBuf, fib: impl Fn(u64) -> u64) -> impl Fn(u64) -> (PathBuf, String) {
     move |num| {
         let path = dir.join(format!("{num}.txt"));
         let content = (0..num)
-            .map(|x| x.to_string())
+            .map(|x| fib(x).to_string())
             .collect::<Vec<_>>()
             .join("\n");
         (path, content)
@@ -35,9 +35,26 @@ fn proc((path, content): (PathBuf, String)) {
 }
 
 fn serial(dir: PathBuf) {
-    (0..MAX).map(data(dir)).for_each(proc)
+    (0..MAX).map(data(dir, fib_serial)).for_each(proc)
 }
 
 fn parallel(dir: PathBuf) {
-    (0..MAX).into_par_iter().map(data(dir)).for_each(proc)
+    (0..MAX)
+        .into_par_iter()
+        .map(data(dir, fib_parallel))
+        .for_each(proc)
+}
+
+fn fib_serial(num: u64) -> u64 {
+    match num {
+        0 | 1 => num,
+        _ => fib_serial(num - 1) + fib_serial(num - 2),
+    }
+}
+
+fn fib_parallel(num: u64) -> u64 {
+    match num {
+        0 | 1 => num,
+        _ => [num - 2, num - 1].into_par_iter().map(fib_parallel).sum(),
+    }
 }
